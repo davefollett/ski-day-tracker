49df2494580a03d37114d4d2887de6d7121b7a9a
Dave Follett <davefollett@gmail.com>
2020-02-01 15:16:57 -0500

    adding default vue project along the serverless functions

--- functions/starwars.js
+++ functions/starwars.js
@@ -1,16 +1,14 @@
-const axios = require('axios');
+const axios = require('axios')
 
 exports.handler = async (event, context) => {
-  
   let status = 200
-  let body = ""
+  let body = ''
 
   try {
-    const response = await axios.get('https://swapi.co/api/people/1');
-    //console.log(response)
+    const response = await axios.get('https://swapi.co/api/people/1')
+    // console.log(response)
     status = response.status
     body = JSON.stringify(response.data)
-
   } catch (error) {
     status = error.response.status
     body = error.response.statusText
