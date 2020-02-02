b5680615cad5817d5ec57fa294e78893b252afbd
Dave Follett <davefollett@gmail.com>
2020-02-01 21:25:22 -0500

    added a json file to the star wars function to control which person to return.

--- functions/starwars.js
+++ functions/starwars.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
+const people = require('./people.json')
 
 exports.handler = async (event, context) => {
   let status = 200
   let body = ''
 
   try {
-    const response = await axios.get('https://swapi.co/api/people/1')
+    const response = await axios.get(`https://swapi.co/api/people/${people.id}`)
     // console.log(response)
     status = response.status
     body = JSON.stringify(response.data)
